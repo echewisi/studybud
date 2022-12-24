d23383adc72888907a848a2f47718048e77ce756
echewisi <josephechewisi@gmail.com>
2022-12-24 01:57:15 +0100

    add

--- base/views.py
+++ base/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import HttpResponseRedirect, render, redirect
 from django.urls import reverse
-from .models import Room, Topic
+from .models import Room, Topic, Message
 from .forms import RoomForm
 from django.db.models import Q
 from django.contrib.auth.models import User
@@ -9,6 +9,7 @@ from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
+
 
 def loginpage(request):
     page= 'login'
@@ -56,8 +57,20 @@ def home(request):
 
 
 def room(request, pk):
+    
     room = Room.objects.get(id=pk)
-    context = {'room': room}
+    room_messages= room.message_set.all().order_by('-created')
+    participants= room.participants.all()
+    if request.method== 'POST':
+        valid_message= Message.objects.create(
+            user= request.user,
+            room= room,
+            body= request.POST.get('body'), #the 'body' here refers to value stated in the 'name' parameter of the form in the template.  
+
+        )
+        room.participants.add(request.user)
+        return redirect('room', pk= room.id)
+    context = {'room': room, 'room_messages': room_messages, 'participants':participants}
     return render(request, 'base/room.html', context)
 
 
