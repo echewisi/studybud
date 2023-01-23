6c8aae87b61965a4926c9e5c4ec651c3ea042e16
echewisi <josephechewisi@gmail.com>
2023-01-23 22:17:09 +0100

    monday

--- base/views.py
+++ base/views.py
@@ -82,7 +82,7 @@ def user_profile(request, pk):
     rooms= user.room_set.all()
     room_messages= user.message_set.all()
     topics= Topic.objects.all()
-    context= {'user': user, 'rooms':rooms}
+    context= {'user': user, 'rooms':rooms, 'room_messages': room_messages, 'topics': topics}
     return render(request, 'base/profile.html', context )
 
 
